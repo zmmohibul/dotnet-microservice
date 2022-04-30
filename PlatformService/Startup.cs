b946e622403ebd7f8b048855caf10e3f013eeac5
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-30 06:03:35 +0600

    Platform Service - Dtos

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -34,6 +34,7 @@ namespace PlatformService
             });
             services.AddScoped<IPlatformRepo, PlatformRepo>();
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
@@ -60,6 +61,8 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+
+             PrepDb.PrepPopulation(app);
         }
     }
 }
