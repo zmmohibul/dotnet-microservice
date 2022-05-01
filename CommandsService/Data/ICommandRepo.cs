e1fe1e03e381c30633f51dada9fb3a7d7c3adf43
zmmohibul <18-36427-1@student.aiub.edu>
2022-05-01 22:21:08 +0600

    CommandService Repository

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+         bool SaveChanges();
+         
+         IEnumerable<Platform> GetAllPlatforms();
+         void CreatePlatform(Platform platform);
+         bool PlatformExists(int platformId);
+
+         IEnumerable<Command> GetCommandsForPlatform(int platformId);
+         Command GetCommand(int platformId, int commandId);
+         void CreateCommand(int platformId, Command command);
+    }
+}
